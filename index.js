7611d1069e66454fb07e985b1f4c6c03efac711f
Sean <larcenciel12@gmail.com>
2022-03-29 19:10:54 +0800

    test

--- index.js
+++ index.js
@@ -10,27 +10,32 @@ app.use('/', express.static(__dirname));
         
     console.log(req.body); 
     
-    res.json({
-      "api_GetContext" : {
-          "_id": "5d15f436966b92b943be9219",
-          "index": 5,
-          "guid": "4f60a28d-3eae-4f2d-abd2-184931b57c6e",
-          "isActive": false,
-          "balance": "$1,023.36",
-          "picture": "http://placehold.it/32x32",
-          "age": 23,
-          "eyeColor": "blue",
-          "name": {
-              "first": "Moon",
-              "last": "Petty"
-          },
-          "company": "KONGENE",
-          "email": "moon.petty@kongene.ca",
-          "phone": "+1 (962) 489-3119"
-      }
-  });
+    async function asyncCall() {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      res.json({
+        "api_GetContext" : {
+            "_id": "5d15f436966b92b943be9219",
+            "index": 5,
+            "guid": "4f60a28d-3eae-4f2d-abd2-184931b57c6e",
+            "isActive": false,
+            "balance": "$1,023.36",
+            "picture": "http://placehold.it/32x32",
+            "age": 23,
+            "eyeColor": "blue",
+            "name": {
+                "first": "Moon",
+                "last": "Petty"
+            },
+            "company": "KONGENE",
+            "email": "moon.petty@kongene.ca",
+            "phone": "+1 (962) 489-3119"
+        }
+      });
 
+    }
     
+
+     asyncCall();
 
 
   })
