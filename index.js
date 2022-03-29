2424fdd76d3ebfdb8d4e0a9bbfc15a6e9e60445f
Sean <larcenciel12@gmail.com>
2022-03-29 18:19:48 +0800

    test

--- index.js
+++ index.js
@@ -10,7 +10,25 @@ app.use('/', express.static(__dirname));
         
     console.log(req.body); 
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ Result: 1 }, null, 3));
+    res.send(JSON.stringify({
+      "items": [
+        {
+          "title": "TITLE ONE",
+          "description": "Description 1",
+          "image": "https://www.beautyarmy.com/wp-content/uploads/2019/12/stack.jpg"
+        },
+        {
+          "title": "TITLE TWO",
+          "description": "Description 2",
+          "image": "https://www.salesbabu.com/wp-content/uploads/2014/02/stock.png"
+        },
+        {
+          "title": "TITLE THREE",
+          "description": "Description 3",
+          "image": "https://3.imimg.com/data3/QJ/UW/MY-14248082/stock-inventory-management-system-with-pos-point-of-sale-500x500.jpg"
+        }
+      ]
+     }, null, 3));
     
         
     
